1237c21ed4d7392331cffd54ba540e8c83a83907
GOLD3NMOON <gold3nmoon1@gmail.com>
2024-11-13 03:28:24 -0300

    (update) add keymap for nvim-tree

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,3 +4,5 @@ local opts = { noremap = true, silent = true }
 map('n', '<Leader>u', 'u', opts)
 map('n', '<Leader>r', '<C-r>', opts)
 map('n', '<Leader>l', 'V', opts)
+
+map('n', '<leader>e', ':NvimTreeToggle<CR>', opts)
