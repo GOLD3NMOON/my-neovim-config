8f4bf6a09f4ef8abbec8ca42e9d4367b2e2bfbc5
GOLD3NMOON <gold3nmoon1@gmail.com>
2024-11-13 16:10:22 -0300

    feat(treesitter): add Tree-sitter configuration for syntax parsing

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,15 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  run = ":TSUpdate",
+  config = function()
+    require("nvim-treesitter.configs").setup {
+      highlight = {
+        enable = true,
+      },
+      indent = {
+        enable = true,
+      },
+    }
+  end
+}
+
