07fc593fd09e8afa67b730da694a985f33b1fc55
GOLD3NMOON <gold3nmoon1@gmail.com>
2024-11-13 03:13:38 -0300

    (feat) add keymaps configuration for Neovim

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,6 @@
+local map = vim.api.nvim_set_keymap
+local opts = { noremap = true, silent = true }
+
+map('n', '<Leader>u', 'u', opts)
+map('n', '<Leader>r', '<C-r>', opts)
+map('n', '<Leader>l', 'V', opts)
