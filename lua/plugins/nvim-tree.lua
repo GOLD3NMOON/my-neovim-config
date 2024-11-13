8d6c2c8e5bd96ec435172b6c27d91cb2dd17510d
GOLD3NMOON <gold3nmoon1@gmail.com>
2024-11-13 03:27:36 -0300

    (feat) add nvim-tree configuration

---
+++ lua/plugins/nvim-tree.lua
@@ -0,0 +1,13 @@
+return {
+  "nvim-tree/nvim-tree.lua",
+  opts = {
+    disable_netrw = false,
+    hijack_netrw = true,
+    open_on_tab = false,
+    update_focused_file = {
+      enable = true,
+      update_cwd = false,
+    },
+  }
+}
+
