034a07111451b72b542171d71fc9b468bf94b2bd
GOLD3NMOON <gold3nmoon1@gmail.com>
2024-11-16 09:48:18 -0300

    (fix) Fixed bug involving the <Leader> key

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,5 +5,9 @@ map('n', '<Leader>u', 'u', opts)
 map('n', '<Leader>r', '<C-r>', opts)
 map('n', '<Leader>l', 'V', opts)
 
-map('n', '<leader>e', ':NvimTreeToggle<CR>', opts)
+map('n', '<Leader>e', ':NvimTreeToggle<CR>', opts)
+map('n', '<Leader>f', '<cmd>lua require("telescope.builtin").find_files()<CR>', opts)
+map('n', '<Leader>n', ':Navbuddy<CR>', opts)
 map('n', ':', '<cmd>FineCmdline<CR>', opts)
+map('n', '<Leader>J', ':ToggleTerm<CR>', opts)
+
