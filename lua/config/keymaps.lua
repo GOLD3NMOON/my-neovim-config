fbcdb6166b3c602afb486d432146d68960219f91
GOLD3NMOON <gold3nmoon1@gmail.com>
2024-11-16 10:55:55 -0300

    (sync) all files in /lua/*

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,9 +5,9 @@ map('n', '<Leader>u', 'u', opts)
 map('n', '<Leader>r', '<C-r>', opts)
 map('n', '<Leader>l', 'V', opts)
 
-map('n', '<Leader>e', ':NvimTreeToggle<CR>', opts)
+map('n', '<Leader>e', '<cmd>NvimTreeToggle<CR>', opts)
 map('n', '<Leader>f', '<cmd>lua require("telescope.builtin").find_files()<CR>', opts)
-map('n', '<Leader>n', ':Navbuddy<CR>', opts)
+map('n', '<Leader>n', '<cmd>Navbuddy<CR>', opts)
 map('n', ':', '<cmd>FineCmdline<CR>', opts)
-map('n', '<Leader>J', ':ToggleTerm<CR>', opts)
+map('n', '<Leader>J', '<cmd>ToggleTerm<CR>', opts)
 
