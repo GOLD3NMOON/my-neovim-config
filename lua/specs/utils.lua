e78c15b9a95fa150dfa44a9d45d44cf588ac27d4
gold3nmoon <gold3nmoon1@gmail.com>
2025-08-28 21:37:12 -0300

    refactor: separating the configurations from the specs (NvimTree, Meson)

--- lua/specs/utils.lua
+++ lua/specs/utils.lua
@@ -1,6 +1,12 @@
 return {
 	{
 		"nvim-tree/nvim-tree.lua",
-		opts = {}
+		opts = require('configs.nvim-tree')
+	},
+	{
+		"mason-org/mason.nvim",
+		opts = function()
+			return require('configs.mason')
+		end
 	}
 }
