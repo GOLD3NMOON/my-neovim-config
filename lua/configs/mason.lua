97a13304900a87259cda274b07c06a7a490ae937
gold3nmoon <gold3nmoon1@gmail.com>
2025-08-28 21:40:14 -0300

    update: meson config

--- lua/configs/mason.lua
+++ lua/configs/mason.lua
@@ -1,1 +1,12 @@
-return {}
+return {
+  PATH = "skip",
+
+  ui = {
+    icons = {
+      package_pending = " ",
+      package_installed = " ",
+      package_uninstalled = " ",
+    },
+  },
+  max_concurrent_installers = 10,
+}
